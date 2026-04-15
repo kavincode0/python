d340bd0e66cb2fb8b32b356e88b48201736c7429
kavincode0 <kav.sharma0291@gmail.com>
2026-04-15 23:54:58 +0530

    makes_twenty

---
+++ makes_twenty.py
@@ -0,0 +1,9 @@
+#MAKES TWENTY: Given two integers, return True if the sum of the integers is 20 or if one of the integers is 20. If not, return False
+
+def makes_twenty(a,b):
+    if a == 20 or b == 20 or a+b == 20:
+        return True
+    else:
+        return False
+    
+print(makes_twenty(1,19))
